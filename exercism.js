9222f222a5d6e0849696711becffad11b35fd100
Hanna Kim <hannakim91@gmail.com>
2020-11-24 19:04:34 -0700

    Add DiffieHellman - Easy

--- exercism.js
+++ exercism.js
@@ -9,3 +9,43 @@
     }
     return false
   };
+
+  // DiffieHellman
+  
+  export class DiffieHellman {
+    constructor(p, g) {
+      this.p = p,
+      this.g = g,
+      this.isGoodArgs()
+    }
+    isInRange(num) {
+      if ((num < 1) || (num > 9998)) {
+        throw new Error("Numbers not in range")
+      }
+    }
+
+    isPrime(num) {
+      if ((num > 2) && (num % 2 === 0) || (num % 3 === 0)) {
+        throw new Error("Number is not prime")
+      }
+    }
+
+    isGoodArgs() {
+      this.isInRange(this.p)
+      this.isInRange(this.g)
+      this.isPrime(this.p)
+      this.isPrime(this.g)
+    }
+
+    getPublicKeyFromPrivateKey(privateKey) {
+      if (privateKey <= 1 || privateKey >= this.p) {
+        throw new Error("Remove this statement and implement this function");
+      }
+      const publicKey = this.g ** privateKey % this.p
+      return publicKey
+    }
+
+    getSharedSecret(privateKey, publicKey) {
+      return publicKey ** privateKey % this.p
+    }
+  }
