441643bb1b606ada81f88ce5468e75287f5256c1
Hanna Kim <hannakim91@gmail.com>
2020-11-24 19:09:06 -0700

    Add resistor color - easy

--- exercism.js
+++ exercism.js
@@ -65,3 +65,12 @@
       return publicKey ** privateKey % this.p
     }
   }
+
+  // resistorColor
+
+  export const colorCode = (color) => {
+    return COLORS.indexOf(color)
+  };
+  
+  export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+  
