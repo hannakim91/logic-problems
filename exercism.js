e04ae2b3b35837e09bee1fe54e0f01cca1910f20
Hanna Kim <hannakim91@gmail.com>
2020-11-24 19:03:09 -0700

    Add Pangram - Easy

--- exercism.js
+++ exercism.js
@@ -1,1 +1,11 @@
 // EASY
+
+  // Pangram
+  export const isPangram = (string) => {
+    const onlyLetters = string.replace(/(\W|_|\d)/g, '').toLowerCase()
+    const set1 = new Set(onlyLetters)
+    if (set1.size === 26) {
+      return true
+    }
+    return false
+  };
