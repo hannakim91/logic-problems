08839a2e386d2277ff39d2246bf47db364b4799b
Hanna Kim <hannakim91@gmail.com>
2020-12-05 17:23:36 -0700

    Add Stop gninnipS My sdroW! kata

--- code-wars-katas.js
+++ code-wars-katas.js
@@ -91,6 +91,13 @@ matrixAddition(
   //   [6, 4, 4],
   //   [2, 2, 4] ] );
 
+
+// stop gninnipS My sdroW!
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
 // 7
 
 // List Filtering
