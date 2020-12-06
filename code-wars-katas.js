0f63a0274daa72b8e3e928e37d3a8fb272e59c82
Hanna Kim <hannakim91@gmail.com>
2020-12-05 17:16:10 -0700

    Add List Filtering kata

--- code-wars-katas.js
+++ code-wars-katas.js
@@ -93,6 +93,9 @@ matrixAddition(
 
 // 7
 
+// List Filtering
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
 // 8 
 
 // The Feast of Many Beasts https://www.codewars.com/kata/5aa736a455f906981800360d
