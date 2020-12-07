dc88c2ec04d075cda2827ef5c2060c421d38681e
Hanna Kim <hannakim91@gmail.com>
2020-12-07 15:32:41 -0700

    Add Credit Card Mask description

--- code-wars-katas.js
+++ code-wars-katas.js
@@ -143,6 +143,12 @@ spinWords("Just kidding there is still one more")
       return l.filter(elem => typeof elem === 'number' && elem >= 0)
     }
 
+// Credit Card Mask
+
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
 // 8 
 
 // The Feast of Many Beasts https://www.codewars.com/kata/5aa736a455f906981800360d
