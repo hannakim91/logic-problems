6af26c00deeb6e9575d66593a1d9d279bbba9dd4
Hanna Kim <hannakim91@gmail.com>
2020-12-07 15:26:13 -0700

    Add Summation kata description

--- code-wars-katas.js
+++ code-wars-katas.js
@@ -200,3 +200,6 @@ countSheeps(array1)
 // iterate through array with forEach
 // counter that adds 1 if element === true
 // return counter 
+
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
