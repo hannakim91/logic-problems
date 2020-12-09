a2fab27e0a132f6fabae0497b1f42b799362a660
Hanna Kim <hannakim91@gmail.com>
2020-12-09 10:47:42 -0700

    Add roman numeral problem

--- classroom-problems.js
+++ classroom-problems.js
@@ -5,3 +5,12 @@
 // return 6 (buying for $5 and selling for $11, making a $6 profit)
 // stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
 // Returns 6
+
+// 2
+// Given an integer, write a function to return its roman numeral representation.
+
+// For example:
+
+// integerToRoman(1) => “I”
+// integerToRoman(4) => “IV”
+// integerToRoman(49) => “XLIX”
