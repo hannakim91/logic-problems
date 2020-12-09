ef03eb9ee4b7569e9d906abb1e749f4d02db05b5
Hanna Kim <hannakim91@gmail.com>
2020-12-09 10:31:46 -0700

    Add project euler #19

---
+++ project-euler.js
@@ -0,0 +1,21 @@
+// In groups: Break this problem down into smaller pieces
+// Project Euler.net #19
+// You are given the following information,
+// 1 Jan 1900 was a Monday.
+
+// Thirty days has September,
+// April, June and November.
+// All the rest have thirty-one,
+
+// Saving February alone,
+// Which has twenty-eight, rain or shine.
+// And on leap years, twenty-nine.
+
+// A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+// How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+// 10:21
+
+// After you've identified the type of problem and have chosen or been given a data structure to use, the next step is to break down the problem.
+// What data is already given?
+// What data is missing?
+// Can each sentence or portion of the sentence be broken down into a high level piece of code?
