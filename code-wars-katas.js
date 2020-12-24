3eff957723cd22642b11a8804e11463040ff1056
Hanna Kim <hannakim91@gmail.com>
2020-12-24 13:58:49 -0700

    Add prompt Duplicate Encoder

--- code-wars-katas.js
+++ code-wars-katas.js
@@ -131,6 +131,15 @@ spinWords("Welcome")
 spinWords("Hey fellow warriors")
 spinWords("Just kidding there is still one more")
 
+// Duplicate Encoder
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
 // 7
 
 // List Filtering
