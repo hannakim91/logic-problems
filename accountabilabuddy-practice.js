af5c11842796d568b09556daab7b383d78982ee1
Hanna Kim <hannakim91@gmail.com>
2021-02-11 09:33:56 -0700

    Add question from cracking the coding interview - jugs

---
+++ accountabilabuddy-practice.js
@@ -0,0 +1,3 @@
+// Cracking the coding interview questions:
+
+  // Question 1 (More math/logic): You have a 5-quart jug, a 3-quart jug, and an unlimited supply of water (no other measuring cups and you can't measure "Half" of the given jugs). How would you come up with exactly 4-quarts of water?
